160c33ed3bc2f9ad1bc707e807c86f4cced36189
Nick Van Dyck <vandyck.nick@outlook.com>
2026-02-04 21:22:00 +0100

    chore: update flake deps

--- flake.nix
+++ flake.nix
@@ -2,46 +2,54 @@
   description = "website";
 
   inputs = {
-    nixpkgs-terraform.url =
-      "github:stackbuilders/nixpkgs-terraform/f2731398b3c4c7086e215f4adf0bda243977ecb1";
+    nixpkgs-terraform.url = "github:stackbuilders/nixpkgs-terraform";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default";
   };
 
   nixConfig = {
     extra-substituters = "https://nixpkgs-terraform.cachix.org";
-    extra-trusted-public-keys =
-      "nixpkgs-terraform.cachix.org-1:8Sit092rIdAVENA3ZVeH9hzSiqI/jng6JiCrQ1Dmusw=";
+    extra-trusted-public-keys = "nixpkgs-terraform.cachix.org-1:8Sit092rIdAVENA3ZVeH9hzSiqI/jng6JiCrQ1Dmusw=";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-terraform, systems }:
-    let forEachSystem = nixpkgs.lib.genAttrs (import systems);
-    in {
-      devShells = forEachSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-terraform,
+      systems,
+    }:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      devShells = forEachSystem (
+        system:
         let
           pkgs = import nixpkgs {
             inherit system;
             config = {
-              allowUnfreePredicate = pkg:
-                builtins.elem (pkgs.lib.getName pkg) [ "terraform" ];
+              allowUnfreePredicate = pkg: builtins.elem (pkgs.lib.getName pkg) [ "terraform" ];
             };
             overlays = [ nixpkgs-terraform.overlays.default ];
 
           };
-        in {
+        in
+        {
           default = pkgs.mkShell {
             packages = [
               pkgs.gnumake
               pkgs.nodejs_24
               pkgs.oxlint
               pkgs.nodePackages.pnpm
-              pkgs.terraform-versions."1.12"
+              pkgs."terraform-1.14"
               pkgs.tflint
             ];
             shellHook = ''
               make install
             '';
           };
-        });
+        }
+      );
     };
 }
